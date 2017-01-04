be095fcd0d7b3d69d872f9c4871dace88005aa6a
Logan Absher <absherlogan@gmail.com>
2017-01-04 14:43:45 -0800

    added 6th questing to array lists

--- app.js
+++ app.js
@@ -1,8 +1,8 @@
 'use strict';
 var conf = confirm('Would you like to play a guessing game?');
-var question = ['Is Logan\'s favorite color blue?', 'Is Logan 20 years old?', 'Does Logan enjoy long walks on the beach?', 'Is Logan a very cool person?', 'Is Logan a psycopathic murderer?'];
-var responseOne = ['You were right Logan\'s favortie color is blue.', 'Yes, Logan is 20 years old.', 'No Logan hates the beach, it\'s so cold why would you ever go to the beach here!', 'Yes thankyou I am great', 'Maybe.'];
-var responseTwo = ['You were wrong Logan likes the color blue the most', 'Do I really look that old?', 'You were right the beach sucks', 'Wow really?', 'Maybe.'];
+var question = ['Is Logan\'s favorite color blue?', 'Is Logan 20 years old?', 'Does Logan enjoy long walks on the beach?', 'Is Logan a very cool person?', 'Does Logan like the water?', 'Is Logan a psycopathic murderer?'];
+var responseOne = ['You were right Logan\'s favortie color is blue.', 'Yes, Logan is 20 years old.', 'No Logan hates the beach, it\'s so cold why would you ever go to the beach here!', 'Yes thankyou I am great', 'Yes, Logan has swam for 15 years and loves the water.', 'Maybe.'];
+var responseTwo = ['You were wrong Logan likes the color blue the most', 'Do I really look that old?', 'You were right the beach sucks', 'Wow really?', 'I\'m a fish', 'Maybe.'];
 if(conf === true){
   var x = 0;
   for(var index = 5; index > 0; index--){
