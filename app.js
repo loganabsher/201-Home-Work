7e07749961546411190b95eb19f62bb31db0e1d4
Logan Absher <absherlogan@gmail.com>
2017-01-05 11:21:43 -0800

    added 2 new lists, made lists coincide with eachother.

--- app.js
+++ app.js
@@ -7,11 +7,11 @@ while(name.length < 1){
 
 var conf = confirm(name + ', would you like to play a guessing game?');
 
-var question = ['Does Logan Like cats?', 'Is Logan 20 years old?', 'Does Logan enjoy long walks on the beach?', 'Is Logan a very cool person?', 'Does Logan like the water?', 'Is Logan a psycopathic murderer?'];
+var question = ['Does Logan Like cats?', 'Do you think Logan is over 19 years old?', 'Does Logan enjoy long walks on the beach?', 'Is Logan a very cool person?', 'Does Logan like the water?', 'Is Logan a psycopathic murderer?'];
 
 var responseOne = ['#catmasterrace', 'Yes, Logan is 20 years old.', 'No Logan hates the beach, it\'s so cold why would you ever go to the beach here!', 'Yes thank you I am great', 'Yes, Logan has swam for 15 years and loves the water.', 'Maybe.'];
 
-var responseTwo = ['#catsarebetter', 'Do I really look that old?', 'You were right the beach sucks', 'Wow really?', 'I\'m a fish', 'Maybe.'];
+var responseTwo = ['#catsarebetter', 'Do I really look that young?', 'You were right the beach sucks', 'Wow really?', 'I\'m a fish', 'Maybe.'];
 
 if(conf === true){
   var x = 0;
@@ -20,15 +20,17 @@ if(conf === true){
     var answer = prompt(question[x]).toLowerCase();
     if(answer === 'yes' || answer === 'yeah' || answer === 'ya' || answer === 'y' || answer === 'sure' || answer === 'confirm'){
       alert(responseOne[x]);
-      console.log('Questing ' + (x + 1) + ':' + responseOne[x]);
+      console.log('Question ' + (x + 1) + ':' + responseOne[x]);
+      document.write('<p class="document">' + (x + 1) + ') ' + question[x] + ': ' + responseOne[x]);
       if(x != 3){
         total++;
       }
     }
     else{
       alert(responseTwo[x]);
-      console.log('Questing ' + (x + 1) + ':' + responseTwo[x]);
-      if (x === 3){
+      console.log('Question ' + (x + 1) + ':' + responseTwo[x]);
+      document.write('<p class="document">' + (x + 1) + ') ' + question[x] + ': ' + responseTwo[x]);
+      if (x === 3 || x === 6){
         total++;
       }
     }
@@ -48,9 +50,9 @@ if(conf === true){
     }
   }
   if (total < 7){
-    document.write('<p class=".document">You got ' + total + ' out of 7 correct, ' + name + '! Better luck next time! </br>');
+    document.write('<p class=".document">You got ' + total + ' out of 7 correct, ' + name + '! Better luck next time! <br>');
   }
   else{
-    document.write('<p class=".document">GOOD JOB ' + name + '!');
+    document.write('<p class=".document">GOOD JOB ' + name + '! <br>');
   }
 }
