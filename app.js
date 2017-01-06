37ab01e0e4cde5bf349eb2e38bd392b19c99f3f7
Logan Absher <absherlogan@gmail.com>
2017-01-06 11:05:49 -0800

    cleaned up CSS, added yes/no arrays

--- app.js
+++ app.js
@@ -3,13 +3,20 @@
 var total = 0;
 
 var name = prompt('What is your name?');
+console.log('name input: ' + name);
 while(name.length < 1){
+  console.log('invalid name: ' + name);
   name = prompt('Please enter a valid name.');
 }
+
+var yesAnswers = ['yes', 'ya', 'y', 'yeah', 'ye', 'sure', 'confirm'];
+
+var noAnswers = ['no', 'nope', 'n', 'noo'];
 
 var conf = confirm(name + ', would you like to play a guessing game?');
 
 if(conf === true){
+  console.log('game starting');
   total = total + guess();
   total = total + game();
   // var x = 0;
@@ -51,10 +58,12 @@ if(conf === true){
 //   }
 // }
   if (total < 7){
-    document.write('<p class="document">You got ' + total + ' out of 7 correct, ' + name + '! Better luck next time! <br><img src="giphy.gif">');
+    console.log('loss ' + total);
+    document.write('<p class="document">You got ' + total + ' out of 7 correct, ' + name + '! Better luck next time! <br><img src="giphy.gif"></p>');
   }
   else{
-    document.write('<p class="document">GOOD JOB ' + name + '! <br><img src="giphy (4).gif">');
+    console.log('winner ' + total);
+    document.write('<p class="document">GOOD JOB ' + name + '! <br><img src="giphy (4).gif"></p>');
   }
 }
 
@@ -62,21 +71,29 @@ function game() {
   var r = Math.round((Math.random() * 9 + 1));
   var total = 0;
   console.log(r);
+  console.log('for loop starting');
   for(var i = 6; i > 0; i --){
+    console.log('i value ' + i);
     var guess = prompt('Guess a number between 1 & 10');
     if(guess == r){
+      console.log('correct');
       alert('YOU WIN!');
       total++;
       break;
     }
     else if (guess > r) {
+      console.log('lower');
       alert('It is lower');
     } else if (guess < r) {
+      console.log('higher');
       alert('It is higher');
     } else {
+      console.log('invalid character');
       alert('You\'re crazy, that is not a number!');
+      i++;
     }
   }
+  console.log('returning total ' + total);
   return total;
 }
 
@@ -86,20 +103,24 @@ function guess(){
   var responseOne = ['#catmasterrace', 'Yes, Logan is 20 years old.', 'You were right the beach sucks', 'Yes thank you I am great', 'Yes, Logan has swam for 15 years and loves the water.', 'Maybe.'];
 
   var responseTwo = ['#catsarebetter', 'Do I really look that young?', 'No Logan hates the beach, it\'s so cold why would you ever go to the beach here!', 'Wow really?', 'I\'m a fish', 'Maybe.'];
+
   total = 0;
-  for(var index = 0; index < question.length; index++){
-    var answer = prompt(question[index]).toLowerCase();
+  console.log('for loop starting');
+  for(var i = 0; i < question.length; i++){
+    console.log('i value ' + i);
+    var answer = prompt(question[i]).toLowerCase();
     if(answer === 'yes' || answer === 'yeah' || answer === 'ya' || answer === 'y' || answer === 'sure' || answer === 'confirm'){
-      alert(responseOne[index]);
-      console.log('Question ' + (index + 1) + ':' + responseOne[index]);
-      document.write('<p class="document">' + (index + 1) + ') ' + question[index] + ': ' + responseOne[index]);
+      alert(responseOne[i]);
+      console.log('Question ' + (i + 1) + ':' + responseOne[i]);
+      document.write('<p class="document">' + (i + 1) + ') ' + question[i] + ': ' + responseOne[i] + '</p>');
       total++;
     }
     else{
-      alert(responseTwo[index]);
-      console.log('Question ' + (index + 1) + ':' + responseTwo[index]);
-      document.write('<p class="document">' + (inedx + 1) + ') ' + question[index] + ': ' + responseTwo[index]);
+      alert(responseTwo[i]);
+      console.log('Question ' + (i + 1) + ':' + responseTwo[i]);
+      document.write('<p class="document">' + (inedx + 1) + ') ' + question[i] + ': ' + responseTwo[i] + '</p>');
     }
   }
+  console.log('returning total ' + total);
   return total;
 }
