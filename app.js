4792a26a47622a531d7d8d7cd5b7d21ede2752f8
Logan Absher <absherlogan@gmail.com>
2017-01-08 18:25:37 -0800

    fixed issue with overlapping global variables, reconfigured game function, fixed total counter

--- app.js
+++ app.js
@@ -1,7 +1,5 @@
 'use strict';
 
-var total = 0;
-
 var name = prompt('What is your name?');
 console.log('name input: ' + name);
 while(name.length < 1){
@@ -12,47 +10,7 @@ while(name.length < 1){
 var conf = confirm(name + ', would you like to play a guessing game?');
 
 if(conf === true){
-  console.log('game starting');
-  total = total + guess(total);
-  total = total + game(total);
-  // var x = 0;
-  // for(var index = 6; index > 0; index--){
-  //   var answer = prompt(question[x]).toLowerCase();
-  //   if(answer === 'yes' || answer === 'yeah' || answer === 'ya' || answer === 'y' || answer === 'sure' || answer === 'confirm'){
-  //     alert(responseOne[x]);
-  //     console.log('Question ' + (x + 1) + ':' + responseOne[x]);
-  //     document.write('<p class="document">' + (x + 1) + ') ' + question[x] + ': ' + responseOne[x]);
-  //     if(x != 3){
-  //       total++;
-  //       console.log(total);
-  //     }
-  //   }
-  //   else{
-  //     alert(responseTwo[x]);
-  //     console.log('Question ' + (x + 1) + ':' + responseTwo[x]);
-  //     document.write('<p class="document">' + (x + 1) + ') ' + question[x] + ': ' + responseTwo[x]);
-  //     if (x === 3 || x === 6){
-  //       total++;
-  //       console.log(total);
-  //     }
-  //   }
-  //   x++;
-  // }
-// function game() {
-//   var r = Math.round((Math.random() * 9 + 1));
-//   console.log(r);
-//   for(var i = 6; i > 0; i --){
-//     var guess = prompt('Guess a number between 1 & 10');
-//     if(guess == r){
-//       alert('YOU WIN!');
-//       total++;
-//       break;
-//     }
-//     else{
-//       alert('wrong');
-//     }
-//   }
-// }
+  var total = game();
   if (total < 7){
     console.log('loss ' + total);
     document.write('<p class="document">You got ' + total + ' out of 7 correct, ' + name + '! Better luck next time! <br><img src="giphy.gif"></p>');
@@ -62,37 +20,37 @@ if(conf === true){
     document.write('<p class="document">GOOD JOB ' + name + '! <br><img src="giphy (4).gif"></p>');
   }
 }
-
-function game(total) {
+function game() {
+  var result = 0;
   var r = Math.round((Math.random() * 9 + 1));
   console.log(r);
-  console.log('for loop starting');
-  for(var i = 6; i > 0; i --){
-    console.log('i value ' + i);
+  for(var i = 6; i > 0; i--){
     var guess = prompt('Guess a number between 1 & 10');
     if(guess == r){
-      console.log('correct');
-      alert('YOU WIN!');
-      total++;
+      console.log('correct: ' + guess);
+      alert('You win, the answer was ' + guess);
+      result++;
+      return result;
       break;
     }
-    else if (guess > r) {
-      console.log('lower');
-      alert('It is lower');
-    } else if (guess < r) {
-      console.log('higher');
-      alert('It is higher');
-    } else {
-      console.log('invalid character');
+    else if(guess > r){
+      console.log('lower: ' + guess);
+      alert('It is lower: ' + guess);
+    }
+    else if(guess < r) {
+      console.log('higher: ' + guess);
+      alert('It is higher: ' + guess);
+    }
+    else{
+      console.log('invalid character: ' + guess);
       alert('You\'re crazy, that is not a number!');
       i++;
     }
+    return result;
   }
-  console.log('returning total ' + total);
-  return total;
 }
 
-function guess(total){
+function guess(){
   var question = ['Does Logan have a cat?', 'Do you think Logan is over the age of 19?', 'Does Logan enjoy long walks on the beach?', 'Has Logan been outside of the USA?', 'Does Logan like the water?', 'Is Logan a psycopathic murderer?'];
 
   var responseOne = ['#catmasterrace', 'Yes, Logan is 20 years old.', 'No Logan hates the beach, it\'s so cold why would you ever go to the beach here!', 'Yes, Logan has visited Canada,  Mexico, Cambodia and France', 'Yes, Logan has swam for 15 years and loves the water, plus water polo.', 'Maybe...'];
@@ -103,31 +61,4 @@ function guess(total){
 
   var noAnswers = ['no', 'nope', 'n', 'noo'];
 
-  console.log('for loop starting');
-  for(var i = 0; i < question.length; i++){
-    var input = prompt(question[i]).toLowerCase();
-    console.log(question[i]);
-    console.log(i);
-    console.log(yesAnswers);
-    console.log(noAnswers);
-    if(input.includes(yesAnswers)){
-      if(i != 3 || i != 5){
-        console.log(total);
-        total++;
-      }
-      alert(responseOne[i]);
-    }
-    else if(input.includes(noAnswers)){
-      if(i === 3 || i === 5){
-        console.log(total);
-        total++;
-      }
-      alert(responseTwo[i]);
-    }
-    // else{
-    //   console.log('invalid response');
-    //   i--;
-    // }
-  }
-  return total;
 }
